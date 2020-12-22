5465827a7fb3079f1073f72558226a5fccbd87c4
Nathanael Yoewono <nathanaelyoewono@nathanaelsmbp2.home>
2020-12-22 16:41:19 +1100

    first push to heroku

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ import time
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 # interactive input output on date
 @app.callback([Output('title_chosen', 'children'),
